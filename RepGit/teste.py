321e7f95392a1f2b0df206f5011f8aed9482a2c8
Anderson <afs_mao@yahoo.com.br>
2023-03-10 00:17:36 -0300

    initial

--- RepGit/teste.py
+++
@@ -1,8 +0,0 @@
-n1 = input ('digite o primeiro numero ')
-n2 = input ('digite o segundo numero ')
-resultado = n1 + n2
-print (resultado)
-n3 = ('digite terceiro numero ')
-n4 = ('digite quarto numero ')
-res = n3 + n4
-print (res)
